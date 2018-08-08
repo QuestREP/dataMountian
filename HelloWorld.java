941378ceee739b241ff51c0bee0c377e0ed762f7
‘Robert <robert.pound@quest.com>
2018-08-08 09:53:56 -0700

    Copied Matt's code

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,18 @@
 *@author Robert Pound
 *@version 1.0
 */
-public class HelloWorld {
-	private void HelloWorld() {
-		
-	}
-    public static void main(final String[] args) {
-        System.out.println("Hi there world");
-        //Crazy comment
-    }
+public final class HelloWorld {
+   /**
+   This is the constructor.
+   */
+   private HelloWorld() {
+   }
+   /**
+   This is the main program.
+   @param args is empty.
+   */
+   public static void main(final String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
